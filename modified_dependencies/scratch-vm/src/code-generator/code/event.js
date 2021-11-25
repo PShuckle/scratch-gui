492ad287e506c7be809062b360c3adb24dd3e46b
Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
2021-11-25 22:38:01 +1300

    convert key from whenkeypressed block into keycode

--- modified_dependencies/scratch-vm/src/code-generator/code/event.js
+++ modified_dependencies/scratch-vm/src/code-generator/code/event.js
@@ -1,3 +1,5 @@
+const keyToEventCode = require('../key-to-event-code');
+
 class Event {
 
     constructor (generator) {
@@ -20,11 +22,17 @@ class Event {
         var nextBlock = this.generator.activeBlocks[block.next];
         var key = block.fields.KEY_OPTION.value;
 
-        // TODO: use actual keycodes
-        var code = 'window.addEventListener(\'keydown\', (event) => {\nif (event.code === "' 
-        + 'ArrowLeft' + '") {'
-        + this.generator.blockToCode(nextBlock)
-        + '}\n});\n';
+        var code = 'window.addEventListener(\'keydown\', (event) => {\n';
+
+        if (key != 'any') {
+            var eventCode = keyToEventCode[key];
+            code += 'if (event.code === "' + eventCode + '") {\n'
+            + this.generator.blockToCode(nextBlock) + '}\n});\n';
+        }
+        else {
+            code += this.generator.blockToCode(nextBlock) + '\n});\n';
+        }
+
         return code;
     }
 }
