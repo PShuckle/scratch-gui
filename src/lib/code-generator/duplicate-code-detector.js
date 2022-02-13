0699e3fe3c8e33c3a026d24ba788fa0d3c0d89b7
Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
2022-02-13 21:46:25 +1300

    duplicate code detection highlits both relevant blocks

--- src/lib/code-generator/duplicate-code-detector.js
+++ src/lib/code-generator/duplicate-code-detector.js
@@ -14,12 +14,9 @@ export default function detectDuplicateCode(xml) {
     for (let j = 0; j < stacks.length; j++) {
         for (let k = 0; k < j; k++) {
             var subsetData = findLongestCommonSubset(stacks[j], stacks[k]);
-            if (subsetData.endIndex - subsetData.startIndex >= 3) {
-                for (let m = subsetData.startIndex; m <= subsetData.endIndex; m++) {
-                    if (stacks[j][m].node) {
-                        stacks[j][m].node.setAttribute('duplicate', true);
-                    }
-                }
+            if (subsetData.endIndex1 - subsetData.startIndex1 >= 3) {
+                stacks[j][subsetData.startIndex1].node.setAttribute('duplicate', true);
+                stacks[k][subsetData.startIndex2].node.setAttribute('duplicate', true);
             }
         }
     }
@@ -35,11 +32,11 @@ function generateBlocksArray(topBlock) {
 function addBlockToArray(blocksArray, block) {
     const childNodes = block.childNodes;
     const type = block.getAttribute('type');
-    blocksArray.push({type: type, node: block});
+    blocksArray.push({ type: type, node: block });
     for (let i = 0; i < childNodes.length; i++) {
         if (childNodes[i].nodeName.toLowerCase() == 'statement') {
             addBlockToArray(blocksArray, childNodes[i].firstChild);
-            blocksArray.push({type: 'statement_end'});
+            blocksArray.push({ type: 'statement_end' });
         } else if (childNodes[i].nodeName.toLowerCase() == 'next') {
             addBlockToArray(blocksArray, childNodes[i].firstChild);
         }
@@ -49,7 +46,8 @@ function addBlockToArray(blocksArray, block) {
 function findLongestCommonSubset(stack1, stack2) {
     var longestSubsetLookup = make2dArray(stack1.length, stack2.length);
     var longestSubsetLength = 0;
-    var endIndex = 0;
+    var endIndex1 = 0;
+    var endIndex2 = 0;
     for (let i = 0; i < stack1.length; i++) {
         for (let j = 0; j < stack2.length; j++) {
             if (stack1[i].type == stack2[j].type) {
@@ -61,7 +59,8 @@ function findLongestCommonSubset(stack1, stack2) {
                 }
                 if (longestSubsetLookup[i][j] > longestSubsetLength) {
                     longestSubsetLength = longestSubsetLookup[i][j];
-                    endIndex = i;
+                    endIndex1 = i;
+                    endIndex2 = j;
                 }
             } else {
                 longestSubsetLookup[i][j] = 0;
@@ -69,12 +68,17 @@ function findLongestCommonSubset(stack1, stack2) {
         }
     }
 
-    return {endIndex: endIndex, startIndex: endIndex - longestSubsetLength + 1};
+    return {
+        endIndex1: endIndex1,
+        startIndex1: endIndex1 - longestSubsetLength + 1,
+        endIndex2: endIndex2,
+        startIndex2: endIndex2 - longestSubsetLength + 1
+    };
 }
 
 function make2dArray(x, y) {
     var arr = [];
-    for(let i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
         arr.push(new Array(y));
     }
     return arr;
