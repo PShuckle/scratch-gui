9f1bc07e34e0c74efd18f185617257961a135557
Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
2022-02-15 16:56:58 +1300

    add commenting to webscraper

--- src/lib/project-scraper/project-scraper.js
+++ src/lib/project-scraper/project-scraper.js
@@ -1,19 +1,20 @@
+// adapted from https://forkphorus.github.io/sb-downloader/
+
 import webdriver, {
     By
 } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
 import chromedriver from 'chromedriver';
-// const {
-//     By
-// } = webdriver;
 
 const downloadProjects = async () => {
+    // set options for Chrome browser
     const chromeCapabilities = webdriver.Capabilities.chrome();
 
     chromeCapabilities.set('goog:chromeOptions', {
         'args': ['disable-infobars'],
         'prefs': {
             'download': {
+                // CHANGE THIS
                 'default_directory': 'C:\\Users\\nicho\\Downloads\\scratch-downloads',
                 'prompt_for_download': 'false'
             },
@@ -27,12 +28,15 @@ const downloadProjects = async () => {
 
     const projectURLs = [];
 
+    // get list of projects by search term
     if (process.argv[2] == 'search') {
+        // search for the search term on scratch
         var searchURL = 'https://scratch.mit.edu/search/projects?q=' + process.argv[3];
         await driver.get(searchURL);
 
         const buttons = await driver.findElements(By.className('button'));
 
+        // find the button that allows loading more projects
         var nextButton;
 
         for (let i = 0; i < buttons.length; i++) {
@@ -42,6 +46,7 @@ const downloadProjects = async () => {
             }
         }
 
+        // create list of IDs of matching projects
         var projects = await driver.findElements(By.className('thumbnail-image'));
 
         while (projects.length < process.argv[4]) {
@@ -51,19 +56,15 @@ const downloadProjects = async () => {
 
         for (let i = 0; i < projects.length; i++) {
             var href = await projects[i].getAttribute('href');
-            // projectURLs.push(href.match(/\d+/)[0]);
             projectURLs.push(href.substring(0, href.length - 1));
         }
 
-
-
     } else if (process.argv[2] == 'url') {
+        // get ID of a single project
         projectURLs.push(process.argv[3]);
     }
 
     await driver.get(process.cwd() + '\\src\\lib\\project-scraper\\index.html');
-
-    // const inputBar = await driver.findElement(By.id('project-select'));
 
     const downloadButtons = await driver.findElements(By.className('download-button'));
 
@@ -71,17 +72,13 @@ const downloadProjects = async () => {
 
     for (let i = 0; i < projectURLs.length; i++) {
         var url = projectURLs[i];
+
+        // write the project id into the user input box
         var script = "document.getElementById('project-select').setAttribute('value', '" + url + "');";
         await driver.executeScript(script);
 
-
         generateSb3Button.click();
-
-        // download(url, 'sb3');
-
     };
-
-    // driver.close();
 }
 
 downloadProjects();
